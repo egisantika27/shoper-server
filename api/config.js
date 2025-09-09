782efac2d92ecd55f70f1885db6dfdcadf649d71
egisantika27 <egi.santika27@gmail.com>
2025-09-10 03:52:09 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -42,10 +42,7 @@ export default function handler(request) {
         "pixelId": "1289968729543730",
         "accessToken": "EAASnNwd76JMBPeTZBetttpTh8zhGeZAqUDnqumD9yzzC3fiRCD5Uyxl57HEuOkWaQvJpC3CqhbtU8Qk4EUC5ZCnYud1KrEUgZCD6yKEPRE9bTlzUHVZCP9pd8WdS2amCSDe1r4frr90yEXPrLQ5kMCEw7D2KVREQJIwUOBUs1CXyIlYMZCdeWoJrDbIOr2Fgy3YQZDZD"
     },
-  	"ga4Credentials": {
-	    "measurementId": "G-Y773PEQWGJ",
-	    "apiSecret": "PjDXc3V3RWWnev2KqBuyrg"
-  },  
+
     "dynamicUrls": {
         "landingPageBaseUrl": "https://shoper-beranda.vercel.app",
         "tutorial": "https://shoper.myscalev.com/shoper-shopee-product-research-tool",
@@ -64,3 +61,4 @@ export default function handler(request) {
   });
 
 }
+
