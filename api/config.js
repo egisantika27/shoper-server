ae2a74a29b87983750be07e83e9445159db37011
egisantika27 <egi.santika27@gmail.com>
2026-07-19 22:31:00 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -44,7 +44,7 @@ export default function handler(request) {
       "detailTitleSectionMobile": ".ezTNz_",
 	  // --- Halaman Kategori/Pencarian ---
 	  "productCardLink": "a[href*='-i.']",
-      "productCardInfoContainer": "div.p-2.flex-1.flex.flex-col.justify-between",
+      "productCardInfoContainer": "div[class*='flex-col']",
       // --- Halaman Produk Serupa (Analyzer) ---
 	  "similarProductContainer": ".VwiYq1", // Desktop Anchor
 	  // ✅ BARU: Penanda/Anchor "Produk Serupa" (Mobile)
